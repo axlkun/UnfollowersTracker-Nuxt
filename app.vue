d6c237d509cd8f546b90fd0f934f2a41a27eda23
axlkun <axelcruz.dev@gmail.com>
2023-12-11 17:51:22 -0600

    implement cookies consent

--- app.vue
+++ app.vue
@@ -7,7 +7,31 @@
     <main>
       <router-view :blogEntry="blogEntry"></router-view>
     </main>
-    
+
+    <ClientOnly fallback-tag="span" fallback="Loading comments...">
+
+      <div class="text-center">
+        <v-bottom-sheet v-model="sheet" v-if="!cookiesAccepted">
+          <v-card class="text-center">
+            <v-card-text>
+              <div>
+                Usamos cookies para mejorar tu experiencia y mostrar anuncios. Al navegar en el sitio, aceptas nuestra
+                Política de cookies. Puedes cambiar la configuración en tu navegador en cualquier momento.
+              </div>
+            </v-card-text>
+            <v-card-actions class="mx-auto">
+              <v-btn variant="text" color="pink" href="/politica-de-cookies-unfollowerstracker">
+                Leer más
+              </v-btn>
+              <v-btn variant="tonal" color="pink" @click="acceptCookies">
+                Aceptar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-bottom-sheet>
+      </div>
+    </ClientOnly>
+
     <myFooter :blogEntry="blogEntry"></myFooter>
 
   </v-app>
@@ -20,19 +44,33 @@ import myHeader from './components/Header.vue';
 import myFooter from './components/Footer.vue';
 
 const blogEntry = ref([]);
+const sheet = ref(true);
+const cookiesAccepted = ref(false);
 
-const getArticles  = async () => {
+const acceptCookies = () => {
+  cookiesAccepted.value = true;
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('cookiesAccepted', 'true');
+  }
+};
+
+const getArticles = async () => {
   api.get('/api/articles?limit=3')
-        .then(response => {
-          blogEntry.value = response.data.data;
-        })
-        .catch(error => {
-          console.error('Error al hacer la solicitud GET:', error);
-        });
+    .then(response => {
+      blogEntry.value = response.data.data;
+    })
+    .catch(error => {
+      console.error('Error al hacer la solicitud GET:', error);
+    });
 }
-  
+
 onMounted(() => {
-    getArticles();
+  getArticles();
+
+  if (typeof localStorage !== 'undefined') {
+    cookiesAccepted.value = localStorage.getItem('cookiesAccepted') === 'true';
+    sheet.value = !cookiesAccepted.value;
+  }
 });
 
 
