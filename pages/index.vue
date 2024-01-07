7d57386b7b18522fe32dab287e6ced2078e30de0
axlkun <axelcruz.dev@gmail.com>
2024-01-06 20:15:05 -0600

    update index title

--- pages/index.vue
+++ pages/index.vue
@@ -38,12 +38,12 @@ useSeoMeta({
 
   robots: 'index, follow',
 
-  title: 'UnfollowersTracker | Home',
+  title: 'UnfollowersTracker | Discover your Instagram Unfollowers',
   author: 'Axel Cruz',
   description: 'Discover who doesn\'t follow you back on Instagram. Free tool to manage your follower list without passwords.',
   keywords: 'Instagram, followers, non-followers, free tool, follower management, Instagram optimization, reach on Instagram, no password, follower tracking, follower analytics, Instagram statistics, follower monitoring.',
 
-  ogTitle: 'UnfollowersTracker',
+  ogTitle: 'UnfollowersTracker | Discover your Instagram Unfollowers',
   ogDescription: 'Discover who doesn\'t follow you back on Instagram. Free tool to manage your follower list without passwords.',
   ogImage: 'https://unfollowerstracker.com/unfollowers-og-image.png',
   ogUrl: 'https://unfollowerstracker.com/',
