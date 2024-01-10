1a831fa9709a7131e38ad5457f2b254424f91620
axlkun <axelcruz.dev@gmail.com>
2024-01-09 18:05:54 -0600

    update video tutorial

--- components/VideoTutorial.vue
+++ components/VideoTutorial.vue
@@ -8,7 +8,7 @@
 
         <v-sheet class="video-container bg-transparent">
     
-                <iframe class="video" src="https://www.youtube.com/embed/GxOowXWueoo" title="Descubre quién no te sigue en Instagram SIN riesgos de iniciar sesión | Unfollowers Tracker" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe class="video" src="https://www.youtube.com/embed/VEdsePVLgUs" title="Who doesn&#39;t follow me back on Instagram? | Free tool without the risk of logging in Instagram" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 
         </v-sheet>
 
