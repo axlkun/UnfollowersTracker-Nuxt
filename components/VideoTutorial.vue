39f505d13390279676455233c2c6ba64aa666fe4
axlkun <axelcruz.dev@gmail.com>
2024-01-12 18:00:47 -0600

    improve text for SEO

--- components/VideoTutorial.vue
+++ components/VideoTutorial.vue
@@ -2,8 +2,8 @@
 
     <v-sheet class="d-flex flex-column align-center justify-center background-custom" id="steps" style="min-height: 100vh;">
         <v-sheet class="pa-5 text-center bg-transparent custom-sheet">
-            <h2 class="text-h5 text-md-h4 ma-1 font-weight-black text-pink">Video tutorial: How to Use Our Tool</h2>
-            <p class="text-h7 text-md-h6 ma-5 font-weight-bold text-grey-darken-1">Learn step by step how to make the most of <span class="text-pink">Unfollowers Tracker</span> to discover who doesn't follow you back on Instagram.</p>
+            <h2 class="text-h5 text-md-h4 ma-1 font-weight-black text-pink">How to know who unfollowed me on Instagram</h2>
+            <p class="text-h7 text-md-h6 ma-5 font-weight-bold text-grey-darken-1">Learn step by step how to use the <span class="text-pink">Unfollowers Tracker</span> tool to discover who doesn't follow you back on Instagram.</p>
         </v-sheet>
 
         <v-sheet class="video-container bg-transparent">
