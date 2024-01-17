00c5f84aecb211652f198410c3e5f72cbd34fbee
axlkun <axelcruz.dev@gmail.com>
2024-01-16 20:34:02 -0600

    new styles for new hero section

--- components/HomeSection.vue
+++ components/HomeSection.vue
@@ -15,7 +15,7 @@
                     </h1>
 
                     <h2 class="text-subtitle-1 text-center text-md-left text-grey-darken-1 mb-5 font-weight-bold">
-                        Free App to see who Unfollowed you on Instagram. No passwords needed. Easily manage your social circle. .
+                        Free App to see who Unfollowed you on Instagram. No passwords needed. Easily manage your social circle
                     </h2>
 
                     <v-sheet class="d-flex flex-md-row flex-column align-center bg-grey-lighten-4">
@@ -52,8 +52,8 @@ import imgHome from '../assets/hero.png';
     margin: 0 auto;
     background-color: transparent;
 
-    @media only screen and (min-width: 960px) {
-        margin-top: -30px;
+    @media only screen and (min-width: 1280px) {
+        margin-top: -40px;
       }
 }
 
